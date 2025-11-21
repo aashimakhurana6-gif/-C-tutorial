d350492c202c0e190014517624fcacf2eb33c04a
Aashima Khurana <aashimakhurana6@gmail.com>
2025-11-21 22:18:21 +0530

    Add files via upload

---
+++ CH-1_Q5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int decimal;
+    printf("Enter a decimal\n");
+    scanf("%d", &decimal);
+
+    printf("Hexadecimal IS %X", decimal);
+    
+    return 0;
+}
---
+++ CH-1_Q6.c
@@ -0,0 +1,9 @@
+//wap to compute celcius to fahrenheit
+#include<stdio.h>
+
+int main(){
+    float c = 37;
+    float f = ((9.0/5.0)*c) + 32;
+    printf("the value in fahrenheit is %f", f);
+    return 0;
+} 
