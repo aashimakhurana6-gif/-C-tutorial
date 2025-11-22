8499cae8f41c152610bb7eae4145a5a9bb04e341
Aashima Khurana <aashimakhurana6@gmail.com>
2025-11-22 23:23:37 +0530

    Add files via upload

---
+++ CH-1_Q7.c
@@ -0,0 +1,17 @@
+//Wap to find the value of force
+#include<stdio.h>
+
+float force(float);
+
+float force(float mass){
+
+return mass*9.8;
+}
+
+
+int main(){ 
+    int m = 45;
+printf("the value of force is  %.2f\n", force(m));
+
+return 0;
+}
