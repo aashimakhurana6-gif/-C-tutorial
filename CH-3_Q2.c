efd8b2c217a4d071864a5986aff90e749bd668cf
Aashima Khurana <aashimakhurana6@gmail.com>
2025-11-27 15:24:03 +0530

    Add files via upload

---
+++ CH-3_Q2.c
@@ -0,0 +1,15 @@
+//wap to check whether an integer variable a is equal to 11. If a is 11, print "i am 11", otherwise print "i am not 11".
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    if(a = 11){
+        printf("i am 11");
+    }
+    else{
+        printf("i am not 11");
+    }
+
+
+    return 0;
+}
