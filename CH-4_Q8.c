7ca7e7a84457ce6e6ef10ca1e53c1b6942e530b3
Aashima Khurana <aashimakhurana6@gmail.com>
2026-01-12 21:53:56 +0530

    Add files via upload

---
+++ CH-4_Q8.c
@@ -0,0 +1,21 @@
+//Write a C program to check whether a given number is prime or not using a for loop.
+#include<stdio.h>
+
+int main(){
+
+    int n = 6;
+    int prime = 0;
+    for(int i = 2; i<n; i++)
+    {
+        if(n%i == 0){
+            prime = 1;
+        }
+    }
+    if(prime){
+        printf("%d is not prime\n", n);
+    }
+    else{
+        printf("%d is prime\n", n);
+    }
+    return 0;
+}  
