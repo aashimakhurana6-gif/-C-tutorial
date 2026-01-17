eb2f1e212897dc8edca834f38c3c54621fb5f75c
Aashima Khurana <aashimakhurana6@gmail.com>
2026-01-17 18:35:01 +0530

    Add files via upload

---
+++ ch5_5.c
@@ -0,0 +1,17 @@
+// Write a C program to find the sum of first n natural numbers using recursion.
+#include<stdio.h>
+
+int sum_natural(int);
+int sum_natural(int n){
+    if (n == 1){
+    return 1;
+}
+return sum_natural(n-1) + n;
+
+}
+
+int main(){
+printf("the sum of 5 natural number is %d", sum_natural(5));
+
+    return 0;
+}
